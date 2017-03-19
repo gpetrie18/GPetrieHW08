694921c0317773ae0683dbf32150e25acbdb83a3
CSOM <csom@bc-macbookpro-38194d.local>
2017-03-19 19:11:07 -0400

    7.3

--- WeatherGift/PageVC.swift
+++ WeatherGift/PageVC.swift
@@ -9,12 +9,68 @@
 import UIKit
 
 class PageVC: UIPageViewController {
+    
+    
+    var currentPage = 0
+    
+    var locationsArray = ["Local City Weather", "Chestnut Hill, MA", "Sydney, Australia", "Uglich, Russia"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        delegate = self
+        dataSource = self
+        
+        setViewControllers([createDetailVC(forPage: 0)], direction: .forward, animated: false, completion: nil)
+        
+        
 
-        // Do any additional setup after loading the view.
+
+    }
+    
+    func createDetailVC(forPage page: Int) -> DetailVC {
+        
+        currentPage = min(max(0, page), locationsArray.count - 1)
+        
+        let detailVC = storyboard!.instantiateViewController(withIdentifier: "DetailVC")
+            as! DetailVC
+        
+        detailVC.locationsArray = locationsArray
+        detailVC.currentPage = currentPage
+        
+        
+        
+        return detailVC
+        
+        
     }
 
 
+}
+
+
+extension PageVC: UIPageViewControllerDataSource, UIPageViewControllerDelegate{
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        
+        
+        if let currentViewController = viewController as? DetailVC {
+            if currentViewController.currentPage < locationsArray.count - 1 {
+                return createDetailVC(forPage: currentViewController.currentPage + 1)
+            }
+        }
+        return nil
+        
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        
+        
+        if let currentViewController = viewController as? DetailVC {
+            if currentViewController.currentPage > 0 {
+                return createDetailVC(forPage: currentViewController.currentPage - 1)
+            }
+        }
+        return nil
+        
+    }
 }
