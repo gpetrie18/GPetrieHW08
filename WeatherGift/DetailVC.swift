35163a4a5709fe8376bf716025156e3ed0d8d0b1
CSOM <csom@bc-macbookpro-38194d.local>
2017-04-01 23:51:52 -0400

    due 4.3.17

--- WeatherGift/DetailVC.swift
+++ WeatherGift/DetailVC.swift
@@ -17,6 +17,7 @@ class DetailVC: UIViewController {
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var summaryLabel: UILabel!
     @IBOutlet weak var currentImage: UIImageView!
+    @IBOutlet weak var tableView: UITableView!
     
     var currentPage = 0
     var locationsArray = [WeatherLocation]()
@@ -26,6 +27,9 @@ class DetailVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
         locationManager.delegate = self
         if currentPage == 0{
             getLocation()
@@ -41,19 +45,29 @@ class DetailVC: UIViewController {
     func updateUserInterface() {
         
         let isHidden = (locationsArray[currentPage].currentTemp == -999.9)
-            temperatureLabel.isHidden = isHidden
-            locationLabel.isHidden = isHidden
+        temperatureLabel.isHidden = isHidden
+        locationLabel.isHidden = isHidden
         
         locationLabel.text = locationsArray[currentPage].name
-        dateLabel.text = locationsArray[currentPage].coordinates
+        dateLabel.text = formatTimeForTimeZone(unixDateToFormat: locationsArray[currentPage].currentTime, timeZoneString: locationsArray[currentPage].timeZone)
+        // dateLabel.text = locationsArray[currentPage].coordinates
         let curTemperature = String(format: "%3.f", locationsArray[currentPage].currentTemp) + "°"
         temperatureLabel.text = curTemperature
         print("%%% curTemperature inside updateUserInterface = \(curTemperature)")
         summaryLabel.text = locationsArray[currentPage].dailySummary
-        
+        currentImage.image = UIImage(named: locationsArray[currentPage].currentIcon)
+        tableView.reloadData()
     }
     
-    
+    func formatTimeForTimeZone(unixDateToFormat: TimeInterval, timeZoneString: String) ->
+        String {
+            let usableDate = Date(timeIntervalSince1970: unixDateToFormat)
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "EEEE, MMM dd, y"
+            dateFormatter.timeZone = TimeZone(identifier: timeZoneString)
+            let dateString = dateFormatter.string(from: usableDate)
+            return dateString
+    }
     
     
 }
@@ -111,9 +125,9 @@ extension DetailVC: CLLocationManagerDelegate {
                 self.locationsArray[0].name = place
                 self.locationsArray[0].coordinates = currentLat + "," + currentLong
                 self.locationsArray[0].getWeather{
-                     self.updateUserInterface()
+                    self.updateUserInterface()
                 }
-               
+                
                 
             })
             locationManager.stopUpdatingLocation()
@@ -122,5 +136,24 @@ extension DetailVC: CLLocationManagerDelegate {
     
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
         print("error getting location - error code \(error)")
+    }
+    
+
+}
+
+extension DetailVC: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return locationsArray[currentPage].dailyForecastArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "DayWeatherCell") as!
+            DayWeatherCell
+        cell.configureTableCell(dailyForecast: self.locationsArray[currentPage].dailyForecastArray[indexPath.row], timeZone: self.locationsArray[currentPage].timeZone)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
     }
 }
--- WeatherGift/WeatherLocation.swift
+++ WeatherGift/WeatherLocation.swift
@@ -11,10 +11,24 @@ import Alamofire
 import SwiftyJSON
 
 class WeatherLocation {
+    
+    struct DailyForecast{
+        var dailyMaxTemp: Double
+        var dailyMinTemp: Double
+        var dailySummary: String
+        var dailyDate: Double
+        var dailyIcon: String
+    }
+    
     var name = ""
     var coordinates = ""
     var currentTemp = -999.9
     var dailySummary = ""
+    var currentIcon = ""
+    var currentTime = 0.0
+    var timeZone = ""
+    var dailyForecastArray = [DailyForecast]()
+    
     
     func getWeather(completed: @escaping () -> ()) {
         
@@ -24,11 +38,57 @@ class WeatherLocation {
             switch response.result {
             case .success(let value):
                 let json = JSON(value)
+               
+                if let temperature = json["currently"]["temperature"].double {
+                    print("TEMP inside getWeather = \(temperature)")
+                    self.currentTemp = temperature
+                }else {
+                    print("could not return a temperature!")
+                }
+                
                 if let summary = json["daily"]["summary"].string {
                     print("SUMMARY inside getWeather = \(summary)")
                     self.dailySummary = summary
                 }else {
-                    print("could not return a temperature!")
+                    print("could not return a summary!")
+                }
+                
+                if let icon = json["currently"]["icon"].string {
+                    print("ICON inside getWeather = \(icon)")
+                    self.currentIcon = icon
+                }else {
+                    print("could not return a icon!")
+                }
+                
+                if let time = json["currently"]["time"].double {
+                    print("TIME inside getWeather = \(time)")
+                    self.currentTime = time
+                }else {
+                    print("could not return a time!")
+                }
+                
+                if let timeZone = json["timezone"].string {
+                    print("TIMEZONE inside getWeather = \(timeZone)")
+                    self.timeZone = timeZone
+                }else {
+                    print("could not return a timezone!")
+                }
+
+                let dailyDataArray = json["daily"]["data"]
+                self.dailyForecastArray = []
+                let lastDay = min(dailyDataArray.count-1, 6)
+                for day in 1...lastDay {
+                    let maxTemp = json["daily"]["data"][day]["temperatureMax"].doubleValue
+                    let minTemp = json["daily"]["data"][day]["temperatureMin"].doubleValue
+                    let dailySummary = json["daily"]["data"][day]["summary"].stringValue
+                    let dateValue = json["daily"]["data"][day]["time"].doubleValue
+                    let icon = json["daily"]["data"][day]["icon"].stringValue
+                    let iconName = icon.replacingOccurrences(of: "night", with: "day")
+                    
+                    self.dailyForecastArray.append(DailyForecast(dailyMaxTemp: maxTemp, dailyMinTemp: minTemp, dailySummary: dailySummary, dailyDate: dateValue, dailyIcon: icon))
+
+
+
                 }
             case .failure(let error):
                 print(error)
